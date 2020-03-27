6eb62524613e65a59347b3f018dd19ea11772452
Leonardo Ribas Segala <leonardo.segala.atos@caixaseguradora.com.br>
2020-03-27 18:13:34 -0300

    Adicionando Filtro e ordenação na lista

--- src/components/CaseList.js
+++ src/components/CaseList.js
@@ -1,24 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Component } from 'react';
 import { loadCases } from '../reducers/cases';
 import { DataTable, Column} from 'primereact/datatable';
+import { Button } from 'primereact/button';
+import { Link } from 'react-router-dom';
 
-const CaseList = state => {
-    const [cases, setCases] = useState([]);
-    useEffect(() => {
+class CaseList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            cases: []
+        };
+    }
+
+    componentDidMount() {
         loadCases()
             .then(response => {
-                setCases(response.data);
+                this.setState({cases: response.data});
             });
-    }, [] );
-    return (
-        <DataTable  value={cases} emptyMessage="Nenhum caso castrado ainda!" header="Casos">
-            <Column field="pasta" header="Pasta" />
-            <Column field="clientes" header="Clientes" />
-            <Column field="titulo" header="Título" />
-            <Column field="responsavel" header="Responsável" />
-            <Column field="acesso" header="Acesso" />
-        </DataTable>
-    );
+    }
+
+    actionBodyTemplate() {
+        return (
+            <React.Fragment>
+                <Link to="">Alterar</Link>
+                |
+                <Link to="">Excluir</Link>
+            </React.Fragment>
+        );
+    }
+
+    render() {
+        return (
+            <DataTable  value={this.state.cases} emptyMessage="Nenhum caso castrado ainda!" header="Casos">
+                <Column field="pasta" header="Pasta" sortable filter filterMatchMode="contains"/>
+                <Column field="clientes" header="Clientes" sortable filter filterMatchMode="contains"/>
+                <Column field="titulo" header="Título" sortable filter filterMatchMode="contains"/>
+                <Column field="responsavel" header="Responsável" sortable filter filterMatchMode="contains"/>
+                <Column field="acesso" header="Acesso" sortable filter filterMatchMode="contains"/>
+                <Column body={this.actionBodyTemplate} headerStyle={{width: '8em', textAlign: 'center'}} bodyStyle={{textAlign: 'center', overflow: 'visible'}}/>
+            </DataTable>
+        );
+    }
 }
 
 export default CaseList;

4cf099b530467285c4f65d2b2555689ab71e4d65
Leonardo Ribas Segala <leonardo.segala.atos@caixaseguradora.com.br>
2020-03-29 11:19:57 -0300

    Adicionando pesquisa global

--- src/components/CaseList.js
+++ src/components/CaseList.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import { DataTable, Column} from 'primereact/datatable';
 import { acessoToString } from '../actions';
 import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
 import { Link } from 'react-router-dom';
 
 class CaseList extends Component {
     constructor(){
         super();
+        this.state = {
+            searchField: null,
+            globalFilter: null
+        }
         this.actionBodyTemplate = this.actionBodyTemplate.bind(this);
+        this.handleAccessColumn = this.handleAccessColumn.bind(this);
     }
 
     actionBodyTemplate(row) {
@@ -29,15 +35,30 @@ class CaseList extends Component {
     }
 
     render() {
+        let header = (
+            <span className="p-float-label p-inputgroup" style={{margin: '10px 0'}}>
+                <InputText id="searchField" type="text" onInput={(e) => this.setState({searchField: e.target.value})}/>
+                <Button icon="pi pi-search"
+                        onClick={(e) => {
+                            let terms = this.state.searchField;
+                            this.setState({globalFilter: terms})
+                        }}/>
+                <label htmlFor="searchField">Busca</label>
+            </span>
+        );
         return (
-            <DataTable  value={this.props.cases} emptyMessage="Nenhum caso castrado ainda!" header="Casos">
-                <Column field="pasta" header="Pasta" sortable filter filterMatchMode="contains"/>
-                <Column field="clientes" header="Clientes" sortable filter filterMatchMode="contains"/>
-                <Column field="titulo" header="Título" sortable filter filterMatchMode="contains"/>
-                <Column field="responsavel" header="Responsável" sortable filter filterMatchMode="contains"/>
-                <Column body={this.handleAccessColumn} header="Acesso" sortable filter filterMatchMode="contains"/>
-                <Column body={this.actionBodyTemplate} headerStyle={{width: '8em', textAlign: 'center'}} bodyStyle={{textAlign: 'center', overflow: 'visible'}}/>
-            </DataTable>
+            <div className="p-grid form">
+                <div className="p-col-12">
+                    <DataTable  value={this.props.cases} emptyMessage="Nenhum caso castrado ainda!" header={header} globalFilter={this.state.globalFilter}>
+                        <Column field="pasta" header="Pasta" sortable filter filterMatchMode="contains"/>
+                        <Column field="clientes" header="Clientes" sortable filter filterMatchMode="contains"/>
+                        <Column field="titulo" header="Título" sortable filter filterMatchMode="contains"/>
+                        <Column field="responsavel" header="Responsável" sortable/>
+                        <Column body={this.handleAccessColumn} header="Acesso" sortable filter filterMatchMode="contains"/>
+                        <Column body={this.actionBodyTemplate} headerStyle={{width: '8em', textAlign: 'center'}} bodyStyle={{textAlign: 'center', overflow: 'visible'}}/>
+                    </DataTable>
+                </div>
+            </div>
         );
     }
 }
